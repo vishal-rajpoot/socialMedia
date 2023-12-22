bf26a8c4cee35a684ad91de99fe93c282fcf67ed
vishal-rajpoot <vishalrajpoot540@gmail.com>
2023-12-22 21:53:56 +0530

    apis added

---
+++ socialmedia/server/Models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema( {
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesin: String,
+    worksAt: String,
+    relationship: String,
+    follwers: [],
+    following: []
+},
+{timestamps: true}
+)
+
+const userModel =mongoose.model("Users", UserSchema);
+export default userModel;
+
